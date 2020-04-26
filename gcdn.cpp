2f2463d55db9286d7027e435678bbae9e51b86f9
takumi <takumi221b4869@gmail.com>
2020-04-26 10:22:46 +0900

    add file

---
+++ gcdn.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int gcd(int x, int y) { return (x % y) ? gcd(y, x % y) : y; }
+
+int main() {
+  int n, a, b;
+  cin >> n >> a;
+  for (int i = 1; i < n; i++) {
+    cin >> b;
+    a = gcd(a, b);
+  }
+  cout << a;
+
+  return 0;
+}
