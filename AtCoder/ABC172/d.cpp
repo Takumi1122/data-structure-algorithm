04bb68da6c8cdbc3fa37fc4048ad85985a66cfc9
Takumi1122 <takumi221b4869@gmail.com>
2020-06-30 03:32:17 +0900

    add ABC172

---
+++ AtCoder/ABC172/d.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+using namespace std;
+using ll = long long;
+using P = pair<int, int>;
+
+// n <= 10^7
+
+int main() {
+  int n;
+  cin >> n;
+  vector<ll> yaku(n + 1, 1);
+  for (int i = 2; i <= n; i++) {
+    yaku[i]++;
+    for (ll j = i * 2; j <= n; j += i) {
+      yaku[j]++;
+    }
+  }
+  ll sum = 0;
+  for (int i = 1; i <= n; i++) {
+    sum += i * yaku[i];
+  }
+  cout << sum << endl;
+  return 0;
+}
---
+++ cumulative_sum/abc172_c.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+using namespace std;
+using ll = long long;
+using P = pair<int, int>;
+
+// o(n * log(m)) 累積和 + 二分探索
+
+int main() {
+  ll n, m, k;
+  cin >> n >> m >> k;
+  vector<ll> a(n), b(m);
+  rep(i, n) cin >> a[i];
+  rep(i, m) cin >> b[i];
+  vector<ll> s(m + 1, 0);
+  rep(i, m) s[i + 1] = s[i] + b[i];
+  int ans = 0;
+  ll t = 0;
+  for (int i = 0; i <= n; i++) {
+    if (i != 0) {
+      t += a[i - 1];
+    }
+    if (t > k) break;
+    int l = -1;
+    int r = m + 1;
+    while (l + 1 < r) {
+      int mid = (l + r) / 2;
+      if (t + s[mid] <= k) {
+        l = mid;
+      } else {
+        r = mid;
+      }
+    }
+    ans = max(ans, i + l);
+  }
+  cout << ans << endl;
+  return 0;
+}
