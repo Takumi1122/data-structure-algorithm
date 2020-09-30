66fe8b4bdaf5a9de96b3158b32cd59be8fb021e3
Takumi1122 <takumi221b4869@gmail.com>
2020-09-30 19:44:20 +0900

    fix knapsack_dp

--- dp/knapsack_dp/knapsack_no_limit_one_dim.cpp
+++ dp/knapsack_dp/knapsack_no_limit_one_dim.cpp
@@ -4,20 +4,11 @@ using namespace std;
 using ll = long long;
 using P = pair<int, int>;
 
-// ナップサック DP を in-place 化
-// メモリを節約
-
-/*
-    in-place化の条件
-    ・左から順にシーケンシャルに見ていくようなDP
-    ・dp[i][j] から dp[i+1][k] への遷移において、常に k > jとなっているとき
-*/
+// 個数制限なしナップサック問題
+// in-place 化
 
 /*
     参考リンク
-    Educational DP Contest D - Knapsack 1
-      https://atcoder.jp/contests/dp/tasks/dp_d
-
     LIS でも大活躍！ DP の配列使いまわしテクニックを特集
       https://qiita.com/drken/items/68b8503ad4ffb469624c
 */
@@ -30,16 +21,17 @@ void chmax(T &a, T b) {
 int main() {
   int n, W;
   cin >> n >> W;
-  vector<int> weight(n), value(n);
+  vector<ll> weight(n), value(n);
   rep(i, n) cin >> weight[i] >> value[i];
 
-  vector<long long> dp(W + 1, 0);
+  vector<ll> dp(W + 1, 0);
   for (int i = 0; i < n; ++i) {
-    for (int w = W; w >= 0; --w) {
+    for (int w = 0; w <= W; ++w) {
       if (w - weight[i] >= 0) {
         chmax(dp[w], dp[w - weight[i]] + value[i]);
       }
     }
   }
+
   cout << dp[W] << endl;
 }
