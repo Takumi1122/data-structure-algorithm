7cd34f8193071b13d13b8f05ac2b38c61fba7317
Takumi1122 <takumi221b4869@gmail.com>
2020-10-12 12:23:08 +0900

    fix hhkb

--- AtCoder/hhkb/d.cpp
+++ AtCoder/hhkb/d.cpp
@@ -4,12 +4,74 @@ using namespace std;
 using ll = long long;
 using P = pair<int, int>;
 
+/*
+    参考リンク
+    HHKB D - Squares
+      https://atcoder.jp/contests/hhkb2020/tasks/hhkb2020_d
+*/
+
+const int mod = 1000000007;
+struct mint {
+  ll x;  // typedef long long ll;
+  mint(ll x = 0) : x((x % mod + mod) % mod) {}
+  mint operator-() const { return mint(-x); }
+  mint& operator+=(const mint a) {
+    if ((x += a.x) >= mod) x -= mod;
+    return *this;
+  }
+  mint& operator-=(const mint a) {
+    if ((x += mod - a.x) >= mod) x -= mod;
+    return *this;
+  }
+  mint& operator*=(const mint a) {
+    (x *= a.x) %= mod;
+    return *this;
+  }
+  mint operator+(const mint a) const {
+    mint res(*this);
+    return res += a;
+  }
+  mint operator-(const mint a) const {
+    mint res(*this);
+    return res -= a;
+  }
+  mint operator*(const mint a) const {
+    mint res(*this);
+    return res *= a;
+  }
+  mint pow(ll t) const {
+    if (!t) return 1;
+    mint a = pow(t >> 1);
+    a *= a;
+    if (t & 1) a *= *this;
+    return a;
+  }
+
+  // for prime mod
+  // フェルマーの小定理から a * a^(p-2) ≡ 1 (mod p) pは素数
+  mint inv() const { return pow(mod - 2); }
+  mint& operator/=(const mint a) { return (*this) *= a.inv(); }
+  mint operator/(const mint a) const {
+    mint res(*this);
+    return res /= a;
+  }
+};
+
 int main() {
   int t;
   cin >> t;
   rep(i, t) {
-    int n, a, b;
+    ll n, a, b;
     cin >> n >> a >> b;
+
+    if (n < a + b) {
+      cout << 0 << endl;
+      continue;
+    }
+
+    mint all = mint(n - a + 1) * (n - a + 1) * (n - b + 1) * (n - b + 1);
+    mint x =
+        mint(n - a + 1) * (n - b + 1) - mint(n - a - b + 2) * (n - a - b + 1);
+    cout << (all - x * x).x << endl;
   }
-  return 0;
 }
--- AtCoder/hhkb/e.cpp
+++ AtCoder/hhkb/e.cpp
@@ -4,20 +4,127 @@ using namespace std;
 using ll = long long;
 using P = pair<int, int>;
 
+/*
+    参考リンク
+    HHKB E - Lamps
+      https://atcoder.jp/contests/hhkb2020/tasks/hhkb2020_e
+*/
+
+// modint
+const int mod = 1000000007;
+struct mint {
+  ll x;  // typedef long long ll;
+  mint(ll x = 0) : x((x % mod + mod) % mod) {}
+  mint operator-() const { return mint(-x); }
+  mint& operator+=(const mint a) {
+    if ((x += a.x) >= mod) x -= mod;
+    return *this;
+  }
+  mint& operator-=(const mint a) {
+    if ((x += mod - a.x) >= mod) x -= mod;
+    return *this;
+  }
+  mint& operator*=(const mint a) {
+    (x *= a.x) %= mod;
+    return *this;
+  }
+  mint operator+(const mint a) const {
+    mint res(*this);
+    return res += a;
+  }
+  mint operator-(const mint a) const {
+    mint res(*this);
+    return res -= a;
+  }
+  mint operator*(const mint a) const {
+    mint res(*this);
+    return res *= a;
+  }
+  mint pow(ll t) const {
+    if (!t) return 1;
+    mint a = pow(t >> 1);
+    a *= a;
+    if (t & 1) a *= *this;
+    return a;
+  }
+
+  // for prime mod
+  // フェルマーの小定理から a * a^(p-2) ≡ 1 (mod p) pは素数
+  mint inv() const { return pow(mod - 2); }
+  mint& operator/=(const mint a) { return (*this) *= a.inv(); }
+  mint operator/(const mint a) const {
+    mint res(*this);
+    return res /= a;
+  }
+};
+
+int Left[2100][2100], Right[2100][2100], Up[2100][2100], Down[2100][2100];
+
 int main() {
   int h, w;
   cin >> h >> w;
-  vector<string> f(h);
-  rep(i, h) cin >> f[i];
-  vector<vector<ll>> nums(h, vector<ll>(w, 0));
+  vector<string> fi(h);
 
-  rep(i, h) rep(j, w) {
-    if (f[i][j] == '.') {
-      nums[rec.first.first][rec.first.second]++;
-      nums[rec.first.first][rec.second.second]--;
-      nums[rec.second.first][rec.first.second]--;
-      nums[rec.second.first][rec.second.second]++;
+  int k = 0;
+  rep(i, h) {
+    cin >> fi[i];
+    rep(j, w) if (fi[i][j] == '.') k++;
+  }
+
+  // 前処理
+  rep(i, h) {
+    int cur = 0;
+    rep(j, w) {
+      if (fi[i][j] == '#')
+        cur = 0;
+      else
+        ++cur;
+      Left[i][j] = cur;
     }
   }
-  return 0;
+
+  rep(i, h) {
+    int cur = 0;
+    for (int j = w - 1; j >= 0; --j) {
+      if (fi[i][j] == '#')
+        cur = 0;
+      else
+        ++cur;
+      Right[i][j] = cur;
+    }
+  }
+
+  rep(j, w) {
+    int cur = 0;
+    rep(i, h) {
+      if (fi[i][j] == '#')
+        cur = 0;
+      else
+        ++cur;
+      Up[i][j] = cur;
+    }
+  }
+
+  rep(j, w) {
+    int cur = 0;
+    for (int i = h - 1; i >= 0; --i) {
+      if (fi[i][j] == '#')
+        cur = 0;
+      else
+        ++cur;
+      Down[i][j] = cur;
+    }
+  }
+
+  // 集計
+  // 各通路マスに対して、それを照らすようなライト設置方法の個数を数え上げて、それを合算する
+  mint res = 0;
+  rep(i, h) {
+    rep(j, w) {
+      if (fi[i][j] == '#') continue;
+      int num = Left[i][j] + Right[i][j] + Up[i][j] + Down[i][j] - 3;
+      res += mint(2).pow(k) - mint(2).pow(k - num);
+    }
+  }
+  cout << res.x << endl;
 }
