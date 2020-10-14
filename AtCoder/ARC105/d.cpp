8f49e84b8a15047bf5c6b2869f0d87412e51cd48
Takumi1122 <takumi221b4869@gmail.com>
2020-10-14 21:18:43 +0900

    fix ARC105

---
+++ AtCoder/ARC105/d.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+using namespace std;
+using ll = long long;
+using P = pair<int, int>;
+
+bool solve() {
+  int n;
+  cin >> n;
+  vector<ll> a(n);
+  rep(i, n) cin >> a[i];
+  sort(a.begin(), a.end());
+
+  if (n % 2 == 1) return false;
+
+  bool exist = true;
+  for (int i = 0; i < n; i += 2) {
+    if (a[i] != a[i + 1]) exist = false;
+  }
+  if (exist)
+    return false;
+  else
+    return true;
+}
+
+int main() {
+  int t;
+  cin >> t;
+  while (t--) {
+    if (solve())
+      cout << "First" << endl;
+    else
+      cout << "Second" << endl;
+  }
+}
