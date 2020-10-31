7128240df8fe7abf16813538b75393a61be30c7b
Takumi1122 <takumi221b4869@gmail.com>
2020-11-01 05:33:54 +0900

    fix mod

--- mod/modpow.cpp
+++ mod/modpow.cpp
@@ -1,9 +1,12 @@
-#include <iostream>
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (n); ++i)
 using namespace std;
+using ll = long long;
+using P = pair<int, int>;
 
 // a^n mod を計算する
-long long modpow(long long a, long long n, long long mod) {
-  long long res = 1;
+ll modpow(ll a, ll n, ll mod) {
+  ll res = 1;
   while (n > 0) {
     if (n & 1) res = res * a % mod;
     a = a * a % mod;
