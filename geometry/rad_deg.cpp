ce89f86385bb167977351188302ba13079e6739a
Takumi1122 <takumi221b4869@gmail.com>
2020-11-03 05:42:46 +0900

    fix geometry

---
+++ geometry/rad_deg.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+using namespace std;
+using ll = long long;
+using P = pair<int, int>;
+
+const double PI = acosl(-1.0);
+double torad(int deg) { return (double)(deg)*PI / 180; }
+double todeg(double ang) { return ang * 180 / PI; }
