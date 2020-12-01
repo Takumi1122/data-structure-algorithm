4c889bb33cdb0fc9b3c56ada8944c8c5d3055460
Takumi1122 <takumi221b4869@gmail.com>
2020-12-01 21:42:00 +0900

    fix hhkb

--- AtCoder/hhkb/c.cpp
+++ AtCoder/hhkb/c.cpp
@@ -4,6 +4,15 @@ using namespace std;
 using ll = long long;
 using P = pair<int, int>;
 
+// Mex: 最小除外数(Minimum excludant)
+// 集合に含まれない最も小さい非負整数を返す関数
+
+/*
+    参考リンク
+    HHKB C - Neq Min
+      https://atcoder.jp/contests/hhkb2020/tasks/hhkb2020_c
+*/
+
 int main() {
   int n;
   cin >> n;
