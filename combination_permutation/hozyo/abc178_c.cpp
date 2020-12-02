27a9247eb412f4fedb60dc6bf980c7be617a8d34
Takumi1122 <takumi221b4869@gmail.com>
2020-12-03 06:14:55 +0900

    fix combination_permutation

---
+++ combination_permutation/hozyo/abc178_c.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+using namespace std;
+using ll = long long;
+using P = pair<int, int>;
+
+/*
+    参考リンク
+    ABC 178 C - Ubiquity
+      https://atcoder.jp/contests/abc178/tasks/abc178_c
+*/
+
+const int mod = 1000000007;
+struct mint {
+  ll x;  // typedef long long ll;
+  mint(ll x = 0) : x((x % mod + mod) % mod) {}
+  mint operator-() const { return mint(-x); }
+  mint& operator+=(const mint a) {
+    if ((x += a.x) >= mod) x -= mod;
+    return *this;
+  }
+  mint& operator-=(const mint a) {
+    if ((x += mod - a.x) >= mod) x -= mod;
+    return *this;
+  }
+  mint& operator*=(const mint a) {
+    (x *= a.x) %= mod;
+    return *this;
+  }
+  mint operator+(const mint a) const {
+    mint res(*this);
+    return res += a;
+  }
+  mint operator-(const mint a) const {
+    mint res(*this);
+    return res -= a;
+  }
+  mint operator*(const mint a) const {
+    mint res(*this);
+    return res *= a;
+  }
+  mint pow(ll t) const {
+    if (!t) return 1;
+    mint a = pow(t >> 1);
+    a *= a;
+    if (t & 1) a *= *this;
+    return a;
+  }
+
+  // for prime mod
+  // フェルマーの小定理から a * a^(p-2) ≡ 1 (mod p) pは素数
+  mint inv() const { return pow(mod - 2); }
+  mint& operator/=(const mint a) { return (*this) *= a.inv(); }
+  mint operator/(const mint a) const {
+    mint res(*this);
+    return res /= a;
+  }
+};
+
+int main() {
+  int n;
+  cin >> n;
+  mint ans = mint(10).pow(n);
+  ans -= mint(9).pow(n) * 2;
+  ans += mint(8).pow(n);
+  cout << ans.x << endl;
+}
