5f6804618165e51befade24363886cb514ef9499
Takumi1122 <takumi221b4869@gmail.com>
2020-12-03 06:17:47 +0900

    fix combination_permutation

--- combination_permutation/zyunkai/zyunkai_permutaion.cpp
+++ combination_permutation/zyunkai/zyunkai_permutaion.cpp
@@ -1,39 +1,29 @@
-#include <algorithm>
-#include <iostream>
-#include <vector>
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (n); ++i)
 using namespace std;
+using ll = long long;
+using P = pair<int, int>;
 
-const int INF = 100000000;  // 十分大きな値
+const int INF = 100000000;
 
 int main() {
-  // 入力
-  int n;  // 都市数
+  int n;
   cin >> n;
-  vector<vector<int> > dist(n, vector<int>(n));  // 各都市間の所要時間
-  for (int i = 0; i < n; ++i) {
-    for (int j = 0; j < n; ++j) {
-      cin >> dist[i][j];
-    }
-  }
+  vector<vector<int> > dist(n, vector<int>(n));
+  rep(i, n) rep(j, n) cin >> dist[i][j];
 
-  // 初期順列
   vector<int> order(n);
-  for (int i = 0; i < n; ++i) order[i] = i;
+  rep(i, n) order[i] = i;
 
-  // 探索
-  int res = INF;
+  int ans = INF;
   do {
-    /* 順序 order についての所要時間を計算 */
     int temp_dist = 0;
     for (int i = 1; i < n; ++i) {
       temp_dist += dist[order[i - 1]][order[i]];
     }
-    /* 暫定値より小さかったら更新 */
-    if (temp_dist < res) {
-      res = temp_dist;
-    }
+    ans = min(ans, temp_dist);
   } while (next_permutation(order.begin(), order.end()));
 
-  // 答えを出力
-  cout << res << endl;
+  cout << ans << endl;
+  return 0;
 }
