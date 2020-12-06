ec08d59a4cfdecfd791938016caf1beed8cceb68
Takumi1122 <takumi221b4869@gmail.com>
2020-12-06 16:40:40 +0900

    fix fps

---
+++ fps/arc110_d.cpp
@@ -0,0 +1,76 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+using namespace std;
+using ll = long long;
+using P = pair<int, int>;
+
+// 二項係数の積
+
+/*
+    参考リンク
+    ARC 110 D - Binomial Coefficient is Fun
+      https://atcoder.jp/contests/arc110/tasks/arc110_d
+*/
+
+const int mod = 1000000007;
+struct mint {
+  ll x;  // typedef long long ll;
+  mint(ll x = 0) : x((x % mod + mod) % mod) {}
+  mint operator-() const { return mint(-x); }
+  mint& operator+=(const mint a) {
+    if ((x += a.x) >= mod) x -= mod;
+    return *this;
+  }
+  mint& operator-=(const mint a) {
+    if ((x += mod - a.x) >= mod) x -= mod;
+    return *this;
+  }
+  mint& operator*=(const mint a) {
+    (x *= a.x) %= mod;
+    return *this;
+  }
+  mint operator+(const mint a) const {
+    mint res(*this);
+    return res += a;
+  }
+  mint operator-(const mint a) const {
+    mint res(*this);
+    return res -= a;
+  }
+  mint operator*(const mint a) const {
+    mint res(*this);
+    return res *= a;
+  }
+  mint pow(ll t) const {
+    if (!t) return 1;
+    mint a = pow(t >> 1);
+    a *= a;
+    if (t & 1) a *= *this;
+    return a;
+  }
+
+  // for prime mod
+  // フェルマーの小定理から a * a^(p-2) ≡ 1 (mod p) pは素数
+  mint inv() const { return pow(mod - 2); }
+  mint& operator/=(const mint a) { return (*this) *= a.inv(); }
+  mint operator/(const mint a) const {
+    mint res(*this);
+    return res /= a;
+  }
+};
+
+int main() {
+  ll n, m;
+  cin >> n >> m;
+  ll s = 0;
+  vector<int> a(n);
+  rep(i, n) cin >> a[i], s += a[i];
+
+  mint ans = 1;
+  rep(i, n + s) {
+    ans *= n + m - i;
+    ans /= n + s - i;
+  }
+  cout << ans.x << endl;
+  return 0;
+}
