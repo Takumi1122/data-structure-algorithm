02ad7cc5d7ac53725a42d1ed530fd80e3f9d3acb
Takumi1122 <takumi221b4869@gmail.com>
2020-12-10 18:39:29 +0900

    fix bit

--- bit/partial_sum/partial_sum.cpp
+++ bit/partial_sum/partial_sum.cpp
@@ -10,17 +10,14 @@ int main() {
   vector<int> a(n);
   rep(i, n) cin >> a[i];
 
-  // 全探索 --- bit は {0, 1, ..., n-1} の 2^n 通りの部分集合全体を動きます。
   bool exist = false;
-  for (int bit = 0; bit < (1 << n); ++bit) {
-    int sum = 0;  // 部分集合の和
+  rep(bit, 1 << n) {
+    int sum = 0;
     rep(i, n) {
-      if (bit & (1 << i)) {  // i が S に入っているなら足す
+      if (bit & (1 << i)) {
         sum += a[i];
       }
     }
-
-    // sum が K に一致するか
     if (sum == k) exist = true;
   }
 
@@ -28,4 +25,5 @@ int main() {
     cout << "Yes" << endl;
   else
     cout << "No" << endl;
+  return 0;
 }
