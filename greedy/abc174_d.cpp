150c2c103a1e29fea09a817409215e6df7b3406d
Takumi1122 <takumi221b4869@gmail.com>
2020-12-13 00:06:07 +0900

    fix greedy

---
+++ greedy/abc174_d.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+using namespace std;
+using ll = long long;
+using P = pair<int, int>;
+
+/*
+    参考リンク
+    ABC 174 D - Alter Altar
+      https://atcoder.jp/contests/abc174/tasks/abc174_d
+*/
+
+int main() {
+  int n;
+  string s;
+  cin >> n >> s;
+
+  // a: R->W, b: W->R
+  int a = 0, b = 0;
+  // 全てが白色と考えた場合
+  rep(i, n) if (s[i] == 'R') a++;
+  int ans = max(a, b);
+  rep(i, n) {
+    // i: 左からi+1個が赤と考えた場合
+    if (s[i] == 'R')
+      --a;
+    else
+      ++b;
+    int now = max(a, b);
+    ans = min(ans, now);
+  }
+
+  cout << ans << endl;
+  return 0;
+}
