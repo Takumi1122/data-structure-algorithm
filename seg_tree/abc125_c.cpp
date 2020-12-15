c66e3af9aadc0cd7f94464ded9a4ca3dbdc41871
Takumi1122 <takumi221b4869@gmail.com>
2020-12-15 21:44:11 +0900

    fix seg_tree

--- seg_tree/abc125_c.cpp
+++ seg_tree/abc125_c.cpp
@@ -4,15 +4,30 @@ using namespace std;
 using ll = long long;
 using P = pair<int, int>;
 
-// 区間上の最小値 Range Minimam Query(RMQ)
+// N個の値の中から1個だけ取り除いたN−1個の整数の最大公約数の最大値を求めよ
+
+/*
+    参考リンク
+    ABC 125 C - GCD on Blackboard
+      https://atcoder.jp/contests/abc125/tasks/abc125_c
+*/
+
+template <class T>
+inline bool chmax(T& a, T b) {
+  if (a < b) {
+    a = b;
+    return 1;
+  }
+  return 0;
+}
 
 template <class Monoid>
 class SegTree {
-  int n;                                       // 葉の数
-  vector<Monoid> data;                         // データを格納するvector
-  Monoid def;                                  // 初期値かつ単位元
-  function<Monoid(Monoid, Monoid)> operation;  // 区間クエリで使う処理
-  function<Monoid(Monoid, Monoid)> update;     // 点更新で使う処理
+  int n;
+  vector<Monoid> data;
+  Monoid def;
+  function<Monoid(Monoid, Monoid)> operation;
+  function<Monoid(Monoid, Monoid)> update;
 
   Monoid _query(int a, int b, int k, int l, int r) {
     if (r <= a || b <= l) return def;
@@ -55,24 +70,28 @@ class SegTree {
   Monoid operator[](int i) { return data[i + n - 1]; }
 };
 
+// O(log(a)) a < b の場合
+ll gcd(ll a, ll b) { return b ? gcd(b, a % b) : a; }
+
 int main() {
-  int n, Q;
-  cin >> n >> Q;
-  // 要素数n・long long型・区間min・点更新
+  int n;
+  cin >> n;
   SegTree<ll> st(
-      n, (1LL << 31) - 1, [](ll a, ll b) { return min(a, b); },
-      [](ll a, ll b) { return b; });
-  rep(q, Q) {
-    int c, x, y;
-    cin >> c >> x >> y;
-    if (c == 0) {
-      // update(x, y)
-      st.change(x, y);
-    } else {
-      // find(s, t)
-      cout << st.query(x, y + 1) << endl;
-    }
+      n, 0, [](ll a, ll b) { return gcd(a, b); }, [](ll a, ll b) { return b; });
+
+  vector<int> a(n);
+  rep(i, n) {
+    cin >> a[i];
+    st.change(i, a[i]);
   }
 
+  ll ans = 0;
+  rep(i, n) {
+    ll l = st.query(0, i);
+    ll r = st.query(i + 1, n);
+    chmax(ans, gcd(l, r));
+  }
+
+  cout << ans << endl;
   return 0;
 }
