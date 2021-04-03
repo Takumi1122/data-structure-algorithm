ae9951b925257b1ef1614ce60ecc39fa45f8cbf1
Takumi1122 <takumi221b4869@gmail.com>
2021-04-03 20:52:02 +0900

    fix bit_dp

--- dp/bit_dp/grouping.cpp
+++ dp/bit_dp/grouping.cpp
@@ -10,23 +10,24 @@ using P = pair<int, int>;
       https://atcoder.jp/contests/dp/tasks/dp_u
 */
 
-int a[20][20];
-ll dp[1 << 16];
-
 int main() {
   int n;
   cin >> n;
+  vector<vector<int>> a(n, vector<int>(n));
   rep(i, n) rep(j, n) cin >> a[i][j];
 
-  for (int s = 1; s < 1 << n; s++) {
-    rep(i, n) rep(j, i) if (s >> i & s >> j & 1) dp[s] += a[i][j];
+  int n2 = 1 << n;
+  vector<ll> dp(n2);
 
-    for (int t = s; t > 0; t = t - 1 & s) {
+  rep(s, n2) {
+    rep(i, n) rep(j, i) if (s >> i & s >> j & 1) dp[s] += a[i][j];
+    // sの部分集合を調べる
+    for (int t = s; t > 0; t = (t - 1) & s) {
       if (t == s) continue;
       dp[s] = max(dp[s], dp[t] + dp[s ^ t]);
     }
   }
 
-  cout << dp[(1 << n) - 1] << endl;
+  cout << dp[n2 - 1] << endl;
   return 0;
 }
