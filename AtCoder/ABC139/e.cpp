24521c7a728a29555f5925a3f3bb265596dbf2cd
Takumi1122 <takumi221b4869@gmail.com>
2021-04-03 11:40:14 +0900

    fix ABC139

--- AtCoder/ABC139/e.cpp
+++ AtCoder/ABC139/e.cpp
@@ -3,8 +3,63 @@
 using namespace std;
 using ll = long long;
 using P = pair<int, int>;
+using Graph = vector<vector<int>>;
+
+/*
+    参考リンク
+    ABC 139 E - League
+      https://atcoder.jp/contests/abc139/tasks/abc139_e
+*/
 
 int main() {
   int n;
+  cin >> n;
+  vector<vector<int>> a(n, vector<int>(n - 1));
+  rep(i, n) {
+    rep(j, n - 1) {
+      cin >> a[i][j];
+      a[i][j]--;
+    }
+    reverse(a[i].begin(), a[i].end());
+  }
+
+  vector<P> q;
+  auto check = [&](int i) {
+    if (a[i].size() == 0) return;
+    int j = a[i].back();
+    if (a[j].size() == 0) return;
+    if (a[j].back() == i) {
+      P p(i, j);
+      if (p.second < p.first) swap(p.first, p.second);
+      q.push_back(p);
+    }
+  };
+  rep(i, n) { check(i); }
+  int day = 0;
+  while (q.size() > 0) {
+    day++;
+    sort(q.begin(), q.end());
+    q.erase(unique(q.begin(), q.end()), q.end());
+    vector<P> prevQ;
+    swap(prevQ, q);
+    for (P p : prevQ) {
+      int i = p.first, j = p.second;
+      a[i].pop_back();
+      a[j].pop_back();
+    }
+    for (P p : prevQ) {
+      int i = p.first, j = p.second;
+      check(i);
+      check(j);
+    }
+  }
+
+  rep(i, n) {
+    if (a[i].size() != 0) {
+      puts("-1");
+      return 0;
+    }
+  }
+  cout << day << endl;
   return 0;
 }
